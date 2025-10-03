b9d60c90fd2c9192ce9a76c672a27e80f71a74c8
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 00:07:12 -0500

    add ANSI color definitions for terminal output

---
+++ src/util/colors.ts
@@ -0,0 +1,87 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+// Base ANSI Colors
+export const Black = "\x1b[30m";
+export const Red = "\x1b[31m";
+export const Green = "\x1b[32m";
+export const Yellow = "\x1b[33m";
+export const Blue = "\x1b[34m";
+export const Magenta = "\x1b[35m";
+export const Cyan = "\x1b[36m";
+export const White = "\x1b[37m";
+
+// Reset
+export const Reset = "\x1b[0m";
+
+// Variants
+export const Bright = {
+    Black: "\x1b[90m",
+    Red: "\x1b[91m",
+    Green: "\x1b[92m",
+    Yellow: "\x1b[93m",
+    Blue: "\x1b[94m",
+    Magenta: "\x1b[95m",
+    Cyan: "\x1b[96m",
+    White: "\x1b[97m",
+};
+
+export const Dim = {
+    Black: "\x1b[2;30m",
+    Red: "\x1b[2;31m",
+    Green: "\x1b[2;32m",
+    Yellow: "\x1b[2;33m",
+    Blue: "\x1b[2;34m",
+    Magenta: "\x1b[2;35m",
+    Cyan: "\x1b[2;36m",
+    White: "\x1b[2;37m",
+    BrightBlack: "\x1b[2;90m",
+    BrightRed: "\x1b[2;91m",
+    BrightGreen: "\x1b[2;92m",
+    BrightYellow: "\x1b[2;93m",
+    BrightBlue: "\x1b[2;94m",
+    BrightMagenta: "\x1b[2;95m",
+    BrightCyan: "\x1b[2;96m",
+    BrightWhite: "\x1b[2;97m",
+};
+
+export const Bold = {
+    Black: "\x1b[1;30m",
+    Red: "\x1b[1;31m",
+    Green: "\x1b[1;32m",
+    Yellow: "\x1b[1;33m",
+    Blue: "\x1b[1;34m",
+    Magenta: "\x1b[1;35m",
+    Cyan: "\x1b[1;36m",
+    White: "\x1b[1;37m",
+    BrightBlack: "\x1b[1;90m",
+    BrightRed: "\x1b[1;91m",
+    BrightGreen: "\x1b[1;92m",
+    BrightYellow: "\x1b[1;93m",
+    BrightBlue: "\x1b[1;94m",
+    BrightMagenta: "\x1b[1;95m",
+    BrightCyan: "\x1b[1;96m",
+    BrightWhite: "\x1b[1;97m",
+};
+
+export const BoldBright = {
+    Black: "\x1b[1;30m",
+    Red: "\x1b[1;31m",
+    Green: "\x1b[1;32m",
+    Yellow: "\x1b[1;33m",
+    Blue: "\x1b[1;34m",
+    Magenta: "\x1b[1;35m",
+    Cyan: "\x1b[1;36m",
+    White: "\x1b[1;37m",
+    BrightBlack: "\x1b[1;90m",
+    BrightRed: "\x1b[1;91m",
+    BrightGreen: "\x1b[1;92m",
+    BrightYellow: "\x1b[1;93m",
+    BrightBlue: "\x1b[1;94m",
+    BrightMagenta: "\x1b[1;95m",
+    BrightCyan: "\x1b[1;96m",
+    BrightWhite: "\x1b[1;97m",
+};
