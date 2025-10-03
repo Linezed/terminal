af00e6f2b8b9b5f6ab65a7de2d2ad08025e1f353
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 09:57:58 -0500

    refactor: simplify Flags method and implement Flag creation in ContextBase

--- src/cli/impl/context.ts
+++ src/cli/impl/context.ts
@@ -5,48 +5,34 @@
  */
 
 import type { Constructor } from "./constructor.js";
-import type Typable from "../interface/typable.js";
-import Types from "../../types.js";
 import type Flag from "../interface/flag.js";
+import IFlag from "./flag.js";
+import type Context from "../interface/context.js";
+import { _NameableBase } from "./nameable.js";
 
 export function _ContextBase<TBase extends Constructor>(Base: TBase) {
-    return class extends Base {
+    return class
+        extends _NameableBase(Base)
+        implements Context
+    {
         /// Flags associated with the context.
         flags: Map<string, Flag> = new Map<string, Flag>();
 
-        /// Get or set the flags associated with the context.
-        Flags(val?: Flag[]): Map<string, Flag> | void
+        /// Get the flags associated with the context.
+        Flags(): Map<string, Flag>
         {
-            // Case 1: getter
-            if (val === undefined) {
-                return this.flags;
-            }
+            return this.flags;
+        }
 
-            // Case 2: setter
-            val.forEach((flag) => {
-                // Make sure the flag isn't already registered
-                if (this.flags.has(flag.name) || (flag.shortcut && this.flags.has(flag.shortcut))) {
-                    throw new Error("Flag already registered");
-                }
+        /// Creates a new flag and adds it to the context.
+        Flag(name: string): Flag
+        {
+            // Create a new flag
+            let f = new IFlag(this, name);
+            this.flags.set(name, f); // Store in map
 
-                // Ensure the flag is of type Typable
-                if ((flag as Typable).Type === undefined) {
-                    (flag as Typable).type = Types.Boolean;
-                } else if ((flag as Typable).Type() === undefined) {
-                    (flag as Typable).type = Types.Boolean;
-                }
-
-                // Set the flag
-                this.flags.set(flag.Name(), flag);
-
-                // Also set the alias
-                const alias = flag.Shortcut();
-                if (!!alias) {
-                    this.flags.set(alias as string, flag);
-                }
-            });
-
-            return;
+            // Return the flag instance
+            return f;
         }
     };
 }
