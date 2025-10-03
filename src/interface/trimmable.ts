d2052431be4c5d013a66ea01db07d0c5ed0a2025
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 22:42:00 -0500

    add Trimmable interface for terminal framework

---
+++ src/interface/trimmable.ts
@@ -0,0 +1,13 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+export default interface Trimmable {
+    /// The shortcut name of the entity.
+    shortcut: string | undefined;
+
+    /// Getter and setter for the short name of the entity.
+    Shortcut(val?: string): string | undefined;
+}
