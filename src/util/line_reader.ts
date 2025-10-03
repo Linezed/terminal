a094c3d6dac6f7efc3ff69a2930fb85554941917
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 23:28:01 -0500

    add LineReader class for reading lines from standard input

---
+++ src/util/line_reader.ts
@@ -0,0 +1,25 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+/// A utility class for reading lines from a string or stream.
+export default class LineReader {
+    /// Reads a line from the standard input.
+    static async Read(): Promise<string> {
+        return new Promise((resolve) => {
+            // Get the standard input and output streams.
+            const stdin = process.stdin;
+
+            // Resume the standard input stream.
+            stdin.resume();
+
+            // Listen for data on the standard input stream.
+            stdin.once('data', function (data) {
+                resolve(data.toString().trim());
+                stdin.pause(); // Pause the stream after reading
+            });
+        });
+    }
+}
