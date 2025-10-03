30af30018f7dcfb8781e502e9a909661edc50fbd
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 02:18:05 -0500

    feat: add type matching function for improved type validation

---
+++ src/util/type_matcher.ts
@@ -0,0 +1,27 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import Types from "../types.js";
+
+export default function MatchType(expected: Types, got: any) {
+    if (typeof got == "string" && expected != Types.String) {
+        throw new Error(
+            "Expected a string"
+        );
+    }
+
+    if (typeof got == "number" && expected != Types.Number) {
+        throw new Error(
+            "Expected a number"
+        );
+    }
+
+    if (typeof got == "boolean" && expected != Types.Boolean) {
+        throw new Error(
+            "Expected a boolean"
+        );
+    }
+}
