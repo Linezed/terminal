a5968d1e773c41c021e8864254bf83cb43d28512
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 22:58:18 -0500

    add ArgvError interface for structured error handling in argument parsing

---
+++ src/interface/argv/error.ts
@@ -0,0 +1,26 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+/// A parsing error.
+import type { ArgvErrorCode } from "./codes.js";
+
+export default interface ArgvError {
+    /// The error code
+    code: ArgvErrorCode;
+    /// The error message
+    message: string;
+    /// Additional details about the error
+    details?: string | undefined;
+
+    /// Getter for the error code
+    Code(): ArgvErrorCode;
+
+    /// Getter for the error message
+    Message(): string;
+
+    /// Getter for additional details about the error
+    Data(): string | undefined;
+}
