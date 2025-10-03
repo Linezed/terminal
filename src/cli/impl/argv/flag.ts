11e1c5e35aa04d827dbf12cb06e60c9e8fad4b75
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 01:36:11 -0500

    refactor: enhance ParseFlag function to support concatenated flags and type conversion

--- src/cli/impl/argv/flag.ts
+++ src/cli/impl/argv/flag.ts
@@ -6,11 +6,138 @@
 
 import type Flag from "../flag.js";
 import type Command from "../../interface/command.js";
+import ArgvException from "./exception.js";
+import ArgvErrorCode from "../../interface/argv/codes.js";
+import Types from "../../../types.js";
+import ConvertToType from "../../../util/converter.js";
+
+function _ThrowUnknownFlag() {
+    throw new ArgvException(ArgvErrorCode.UnknownFlag, "Unknown flag");
+}
+
+function _FindFlag(
+    name: string,
+    cmd: Command | undefined,
+    flags: Map<string, Flag>
+): Flag {
+    // Make sure the flag has been found
+    if (!flags.has(name as string)) {
+        // Last resort: Try to find the flag in the command
+        if (cmd && cmd.flags.has(name as string)) {
+            return cmd.flags.get(name as string) as Flag;
+        }
+
+        _ThrowUnknownFlag();
+    }
+
+    return flags.get(name as string) as Flag;
+
+}
 
 export default function ParseFlag(
     arg: string,
     cmd: Command | undefined,
-    flags: Map<string, Flag>
+    flags: Map<string, Flag>,
+    strings: Map<string, string> = new Map(),
+    bools: Map<string, boolean> = new Map(),
+    numbers: Map<string, number> = new Map()
 ): Flag | undefined {
-    return;
+    let name: string | undefined;
+    let val: string | undefined;
+    let isShort = false;
+
+    // Check for long flags
+    if (arg[1] == "-") {
+        name = arg.slice(2); // Skip the first 2 slashes
+    }
+    // Short flag detected
+    else {
+        isShort = true;
+        // Set the flag name
+        name = arg.slice(1); // Skip the first slash
+    }
+
+    // Check if we have a flag of the type "--flag=val"
+    if (name.includes("=")) {
+        // Split the string
+        let split = name.split("=");
+
+        // Validate the parts gotten
+        if (split.length != 2) {
+            throw new ArgvException(ArgvErrorCode.UnknownFlag, "Invalid flag");
+        }
+
+        // Set the variables
+        name = split[0];
+        val = split[1];
+    }
+
+    // Check for concatenated flags
+    if (!val && isShort && !flags.has(name as string)) {
+        // Get all concatenated flags
+        for (let i = 0; i < (name as string).length; i++) {
+            // Get the char
+            const char = name?.charAt(i) as string;
+
+            // Get the flag
+            let flag = _FindFlag(char, cmd, flags);
+
+            // Make sure the flag is boolean
+            if (flag.Type() != Types.Boolean) {
+                throw new ArgvException(
+                    ArgvErrorCode.UnknownFlag,
+                    "Cannot concatenate non-boolean flags"
+                );
+            }
+
+            // Set the flag to true in boolean flags
+            bools.set(flag.Name(), true);
+        }
+
+        // Return nothing, all flags parsed
+        return;
+    }
+
+    // Get the flag
+    let flag = _FindFlag(name as string, cmd, flags);
+
+    // Check the flag type
+    if (flag.Type() == Types.Boolean) {
+        // Make sure we don't have a value
+        if (val) {
+            throw new ArgvException(
+                ArgvErrorCode.InvalidFlag,
+                "Boolean flags can't have values"
+            );
+        }
+
+        // Set in the boolean flags
+        bools.set(flag.Name(), true);
+        return;
+    }
+
+    // Check if we have a value
+    if (val) {
+        // Try to parse the value's type
+        let result = ConvertToType(flag.Type(), arg);
+
+        // Put the correct flag in the correct map
+        switch (flag.Type()) {
+            case Types.String:
+                strings.set(name as string, result as string);
+                break;
+
+            case Types.Number:
+                numbers.set(name as string, result as number);
+                break;
+
+            case Types.Boolean:
+                bools.set(name as string, result as boolean);
+                break;
+        }
+
+        return; // No return value
+    }
+
+    return flag; // Return the flag
 }
