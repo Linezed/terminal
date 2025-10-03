54673a2a7fe793459b5a29f37af9a630dc205ae0
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 23:17:02 -0500

    add Terminal class methods for buffered output handling

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@
  License: MIT
  */
 
-import * as process from "node:process";
+import process from "node:process";
 import IApp from "./impl/app.js";
+import BufferedOStream from "./buffered_ostream.js";
 
 export default class Terminal {
+    /// The output stream for the terminal.
+    private static OStream = new BufferedOStream(process.stdout, 1024);
+
     /// Get the command-line arguments passed to the Node.js process.
     public static Args(): string[] {
         return process.argv;
@@ -17,4 +21,25 @@ export default class Terminal {
     public static App(name: string): IApp {
         return new IApp(name);
     }
+
+    /// Prints a line to the standard output
+    public static Println(line: string): void {
+        this.OStream.Write(line);
+        this.OStream.Write("\n"); // New line
+    }
+
+    /// Prints to the standard output without a new line
+    public static Print(text: string): void {
+        this.OStream.Write(text);
+    }
+
+    /// Flush the output stream
+    public static Flush(): void {
+        this.OStream.Flush();
+    }
 }
+
+// Flush on exit
+process.on("beforeExit", () => {
+    Terminal.Flush();
+});
