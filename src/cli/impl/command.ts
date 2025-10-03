22fa30e262e7bdc17735b97e7b770c1bcc42f203
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 01:16:32 -0500

    refactor: add owner property to ICommand and update constructor to accept App instance

--- src/cli/impl/command.ts
+++ src/cli/impl/command.ts
@@ -9,16 +9,22 @@ import { _NameableBase } from "./nameable.js";
 import { _TypableBase } from "./typable.js";
 import { _ContextBase } from "./context.js";
 import { _TrimmableBase } from "./trimmable.js";
+import type App from "../interface/app.js";
 
 export class ICommand
     extends _NameableBase(_TypableBase(_ContextBase(_TrimmableBase(class{}))))
     implements Command
 {
+    /// The owner of the command
+    owner: App | undefined;
+
+    /// The command handler
     handler: ((cmd: Command) => void) | null = null;
 
     /// Constructor
-    constructor(name: string) {
+    constructor(app: App, name: string) {
         super();
+        this.owner = app;
         this.name = name;
     }
 
