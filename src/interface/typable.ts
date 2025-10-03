e7ccc5d1ce25a320b3d628ec0686d6fb7c9f44cd
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 22:42:11 -0500

    add Typable interface for terminal framework

---
+++ src/interface/typable.ts
@@ -0,0 +1,15 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import type Types from "../types.js";
+
+export default interface Typable {
+    /// The type of the entity.
+    type: Types;
+
+    /// Get the type of the entity.
+    Type(): Types;
+}
