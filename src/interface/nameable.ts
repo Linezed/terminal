165d177f299a21e4370654d4cc09c1a8b425a291
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-02 22:41:53 -0500

    add Nameable interface for terminal framework

---
+++ src/interface/nameable.ts
@@ -0,0 +1,19 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+export default interface Nameable {
+    /// The name of the entity.
+    name: string;
+
+    /// The description of the entity.
+    description: string | undefined;
+
+    /// Getter for the name of the entity.
+    Name(): string;
+
+    /// Getter and setter for the description of the entity.
+    Description(val?: string): string | undefined;
+}
