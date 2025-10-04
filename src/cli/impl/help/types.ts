5e79e93b4efb2241031de0185dbe58af855626f0
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-03 23:50:36 -0500

    feat: add CreateTypeText function for formatting type information

---
+++ src/cli/impl/help/types.ts
@@ -0,0 +1,17 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import Formatter from "../../../util/formatter.js";
+import StringifyType from "../../../util/type_stringify.js";
+import Types from "../../../types.js";
+
+export default function CreateTypeText(t: Types) {
+    return Formatter.Format(
+        "{Bright.Black} {Bright.Green}\n",
+        "➠ Type:",
+        StringifyType(t)
+    );
+}
