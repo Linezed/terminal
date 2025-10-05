4198d263f6638ab979b7ccd9694c92dcb2954609
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-05 12:29:55 -0500

    refactor: rename requiredFlags variable to required_flags for consistency

--- src/cli/impl/argv/flag.ts
+++ src/cli/impl/argv/flag.ts
@@ -63,7 +63,7 @@ export default function ParseFlag(
     arg: string,
     cmd: Command | undefined,
     flags: Map<string, Flag>,
-    requiredFlags: Set<string>,
+    required_flags: Set<string>,
     strings: Map<string, string> = new Map(),
     bools: Map<string, boolean> = new Map(),
     numbers: Map<string, number> = new Map()
@@ -120,7 +120,7 @@ export default function ParseFlag(
             bools.set(flag.Name(), true);
 
             // Remove from required flags
-            requiredFlags.delete(flag.Name());
+            required_flags.delete(flag.Name());
         }
 
         // Return nothing, all flags parsed
@@ -148,14 +148,14 @@ export default function ParseFlag(
     // Check if we have a value
     if (val) {
         // Remove from required flags
-        requiredFlags.delete(flag.Name());
+        required_flags.delete(flag.Name());
 
         SetFlagValue(flag, val, strings, bools, numbers);
         return; // No return value
     }
 
     // Remove from required flags
-    requiredFlags.delete(flag.Name());
+    required_flags.delete(flag.Name());
 
     return flag; // Return the flag
 }
