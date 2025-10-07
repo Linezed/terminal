d1b97f3ddbd1ce617737204340f8221cfdd08e69
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-07 17:07:30 -0500

    feat: add methods for managing prefixes and formats in Formatter class

--- src/format/formatter.ts
+++ src/format/formatter.ts
@@ -5,6 +5,10 @@
  */
 
 import FormatOutput from "./format.js";
+import type { PrefixFunction } from "./prefix/type.js";
+import prefixes from "./prefix/collection.js";
+import type { BaseFormatFunction } from "./base/type.js";
+import base_fns from "./base/collection.js";
 
 export default class Formatter {
     public static FormatWithProps(
@@ -40,5 +44,45 @@ export default class Formatter {
 
     public static Format(format: string, ...args: any[]): string {
         return this.FormatWithProps(format, null, ...args);
+    }
+
+    public static AddPrefix(name: string, fn: PrefixFunction) {
+        // Make sure the prefix doesn't already exist
+        if (name in prefixes) {
+            throw new Error(`Prefix "${name}" already exists.`);
+        }
+
+        // Add the prefix
+        prefixes.set(name, fn);
+    }
+
+    public static RemovePrefix(name: string) {
+        // Make sure the prefix exists
+        if (!(name in prefixes)) {
+            throw new Error(`Prefix "${name}" does not exist.`);
+        }
+
+        // Remove the prefix
+        prefixes.delete(name);
+    }
+
+    public static AddFormat(name: string, fn: BaseFormatFunction) {
+        // Make sure the format doesn't already exist
+        if (name in base_fns) {
+            throw new Error(`Format "${name}" already exists.`);
+        }
+
+        // Add the format
+        base_fns.set(name, fn);
+    }
+
+    public static RemoveFormat(name: string) {
+        // Make sure the format exists
+        if (!(name in base_fns)) {
+            throw new Error(`Format "${name}" does not exist.`);
+        }
+
+        // Remove the format
+        base_fns.delete(name);
     }
 }
