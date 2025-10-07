448059fb73368b2ad9a2854a4871b447eb3f0e3e
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-07 13:26:52 -0500

    feat: add State class for managing formatting options and properties

---
+++ src/format/state.ts
@@ -0,0 +1,45 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+export class State {
+    /// The color of the string.
+    color: string | undefined = undefined;
+
+    /// The floating point precision.
+    precision: number | undefined = undefined;
+
+    /// Spaces for JSON formatting.
+    json: undefined | number = undefined;
+
+    /// Prop access.
+    prop: string | undefined = undefined;
+
+    /// Text formatting.
+    text = {
+        upper: false,
+        lower: false,
+        title: false,
+        trim: false,
+
+        /// Padding for text.
+        padding: {
+            left: 0,
+            right: 0
+        },
+
+        /// Slicing for text.
+        slice: {
+            start: 0,
+            end: Infinity
+        }
+    };
+
+    /// Date formatting.
+    date = {
+        iso: false,
+        utc: false,
+    }
+}
