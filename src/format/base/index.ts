f704b75b2000827ba06c7c489aa8aa67a45987ee
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-07 13:27:28 -0500

    feat: add FormatBase function for base formatting with prefix and state

---
+++ src/format/base/index.ts
@@ -0,0 +1,14 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import type { State } from "../state.js";
+
+export default function FormatBase(
+    pref: string,
+    state: State
+) {
+
+}
