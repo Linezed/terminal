51bfa518ed84584ebc77d5e0b5fe0fd9d695348e
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-07 13:36:01 -0500

    feat: add FillDatePrefixes function for managing date transformation states

---
+++ src/format/prefix/preset/date.ts
@@ -0,0 +1,19 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import type { PrefixFunction } from "../type.js";
+import ColorKeys from "../../color_keys.js";
+import type { State } from "../../state.js";
+
+export default function FillDatePrefixes(map: Map<string, PrefixFunction>) {
+    map.set("ISO", (_: string, state: State) => {
+        state.date.iso = true;
+    });
+
+    map.set("UTC", (_: string, state: State) => {
+        state.date.utc = true;
+    });
+}
