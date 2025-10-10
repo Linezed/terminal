f3a43106d20911e075b7bda89106e9a5ae064d3f
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-10 18:40:33 -0500

    test: update flag tests to utilize LookupOrder for flag resolution

--- tests/flag.test.ts
+++ tests/flag.test.ts
@@ -8,13 +8,14 @@ import { test } from "uvu";
 import * as assert from "uvu/assert";
 import IArgv from "../src/cli/impl/argv/argv";
 import app from "./app.test";
+import LookupOrder from "../src/cli/interface/argv/lookup_order";
 
 test("parses local and global flags", () => {
     let ctx: IArgv = app.Parse(["t", "-h", "value"]);
     let ctx_2: IArgv = app.Parse(["t", "-h", "value", "-h"]);
-    assert.is(ctx.local.bools.has("help"), true);
-    assert.is(ctx_2.local.bools.has("help"), true);
-    assert.is(ctx_2.global.bools.has("help"), true);
+    assert.is(ctx.Has("help", LookupOrder.LocalOnly), true);
+    assert.is(ctx_2.Has("help", LookupOrder.GlobalOnly), true);
+    assert.is(ctx_2.Has("help", LookupOrder.LocalOnly), true);
 });
 
 test.run();
