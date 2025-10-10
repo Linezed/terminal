6a0ab8d79835e961af1593396fc7e230a8571784
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-10 18:22:37 -0500

    feat: add tests for Formatter.Format function in format.ts

---
+++ tests/format.ts
@@ -0,0 +1,24 @@
+/*
+ @linezed/terminal
+ A simple yet powerful terminal framework for building command-line applications in TypeScript.
+ License: MIT
+ */
+
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import IArgv from "../src/cli/impl/argv/argv";
+import app from "./app.test";
+import { Formatter } from "../src";
+
+test("formats text", () => {
+    let str = Formatter.Format("Hello, {}", "world!");
+    assert.is(str, "Hello, world!");
+
+    str = Formatter.Format("Number: {}, String: {}, Bool: {}", 42, "test", true);
+    assert.is(str, "Number: 42, String: test, Bool: true");
+
+    str = Formatter.Format("No placeholders here.");
+    assert.is(str, "No placeholders here.");
+});
+
+test.run();
