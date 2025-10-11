d16e84424a06c3700fa527d15a8a368ec788a28e
rodrigoo-r <rodrigo@zelixlang.dev>
2025-10-11 10:49:24 -0500

    feat: introduce NamedListenerCollection and OrderedNamedListenerCollection for enhanced handler organization

--- src/format/listener_collection.ts
+++ src/format/listener_collection.ts
@@ -9,9 +9,22 @@ import type { CustomHandlerFunction } from "./custom/type.js";
 export class ListenerCollection {
     highest = [] as CustomHandlerFunction[];
     high = [] as CustomHandlerFunction[];
-    medium = [] as CustomHandlerFunction[];
+    normal = [] as CustomHandlerFunction[];
     low = [] as CustomHandlerFunction[];
     lowest = [] as CustomHandlerFunction[];
+}
+
+export class NamedListenerCollection {
+    highest = new Map<string, CustomHandlerFunction[]>();
+    high = new Map<string, CustomHandlerFunction[]>();
+    normal = new Map<string, CustomHandlerFunction[]>()
+    low = new Map<string, CustomHandlerFunction[]>();
+    lowest = new Map<string, CustomHandlerFunction[]>();
+}
+
+export class OrderedNamedListenerCollection {
+    pre = new NamedListenerCollection();
+    post = new NamedListenerCollection();
 }
 
 export default class OrderedListenerCollection {
